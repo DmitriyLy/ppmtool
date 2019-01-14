3794d899f70ef6b97eab004ce9d94bd235848d7c
DmitriyLy <dmitriy.lisay@gmail.com>
2019-01-14 18:17:36 +0200

    s2 l10

---
+++ serverside/src/main/java/io/agintelligence/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.agintelligence.repositories;
+
+import io.agintelligence.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
