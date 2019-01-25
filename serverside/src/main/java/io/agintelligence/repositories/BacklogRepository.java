5ee59543fe356524d5927da477cc6e9c19b95f02
DmitriyLy <dmitriy.lisay@gmail.com>
2019-01-25 16:27:24 +0200

    s4 l47

---
+++ serverside/src/main/java/io/agintelligence/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package io.agintelligence.repositories;
+
+import io.agintelligence.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
