df7f18c501b0ff957129e1f408ec72e8c51bb288
DmitriyLy <dmitriy.lisay@gmail.com>
2019-01-26 12:14:55 +0200

    s4 l47

--- serverside/src/main/java/io/agintelligence/services/ProjectTaskService.java
+++ serverside/src/main/java/io/agintelligence/services/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package io.agintelligence.services;
 
+import io.agintelligence.domain.Backlog;
 import io.agintelligence.domain.ProjectTask;
 import io.agintelligence.repositories.BacklogRepository;
 import io.agintelligence.repositories.ProjectTaskRepository;
@@ -15,8 +16,29 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask() {
-        return null;
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
+        projectTask.setBacklog(backlog);
+
+        Integer backlogSequence = backlog.getPTSequence();
+        backlogSequence++;
+
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+            projectTask.setPriority(3);
+        }
+
+        if (projectTask.getStatus() == null || projectTask.getStatus() == "") {
+            projectTask.setStatus("TO_DO");
+        }
+
+
+
+        return projectTaskRepository.save(projectTask);
     }
 
 }
