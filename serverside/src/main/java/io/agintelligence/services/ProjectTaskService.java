050ab1c8906f38cfa6d9667508f49050daa57c21
DmitriyLy <dmitriy.lisay@gmail.com>
2019-01-31 18:00:29 +0200

    s4 l49

--- serverside/src/main/java/io/agintelligence/services/ProjectTaskService.java
+++ serverside/src/main/java/io/agintelligence/services/ProjectTaskService.java
@@ -7,6 +7,8 @@ import io.agintelligence.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectTaskService {
 
@@ -42,4 +44,7 @@ public class ProjectTaskService {
         return projectTaskRepository.save(projectTask);
     }
 
+    public List<ProjectTask> findBacklogById(String backlog_id) {
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlog_id);
+    }
 }
