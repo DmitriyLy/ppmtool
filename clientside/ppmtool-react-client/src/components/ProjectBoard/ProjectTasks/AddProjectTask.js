c3e753676ac9f2274aaf84121784ee3f0c1db0ba
DmitriyLy <dmitriy.lisay@gmail.com>
2019-02-13 18:08:08 +0200

    s5 l64

--- clientside/ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
+++ clientside/ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/AddProjectTask.js
@@ -14,13 +14,34 @@ class AddProjectTask extends Component {
 
         this.state = {
             summary: "",
-            acceptanceCriteria: "null",
+            acceptanceCriteria: "",
             status: "",
             priority: 0,
             dueDate: "",
             projectIdentifier: id,
             errors: {}   
         };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });    
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const newTask = {
+            summary: this.state.summary,
+            acceptanceCriteria: this.state.acceptanceCriteria,
+            status: this.state.status,
+            priority: this.state.priority,
+            dueDate: this.state.dueDate    
+        };
+
+        this.props.addProjectTask(this.state.projectIdentifier, newTask, this.props.history);
     }
 
     render() {
@@ -37,30 +58,34 @@ class AddProjectTask extends Component {
                             </Link>
                             <h4 className="display-4 text-center">Add Project Task</h4>
                             <p className="lead text-center">Project Name + Project Code</p>
-                            <form>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
                                     <input type="text" className="form-control form-control-lg" name="summary" 
                                         placeholder="Project Task summary"
-                                        value={this.state.summary} 
+                                        value={this.state.summary}
+                                        onChange={this.onChange} 
                                     />
                                 </div>
                                 <div className="form-group">
                                     <textarea className="form-control form-control-lg" placeholder="Acceptance Criteria" 
                                         name="acceptanceCriteria"
-                                        value={this.state.acceptanceCriteria} 
+                                        value={this.state.acceptanceCriteria}
+                                        onChange={this.onChange} 
                                     ></textarea>
                                 </div>
                                 <h6>Due Date</h6>
                                 <div className="form-group">
                                     <input type="date" className="form-control form-control-lg" 
                                         name="dueDate"
-                                        value={this.state.dueDate} 
+                                        value={this.state.dueDate}
+                                        onChange={this.onChange} 
                                     />
                                 </div>
                                 <div className="form-group">
                                     <select className="form-control form-control-lg" 
                                         name="priority"
                                         value={this.state.priority}
+                                        onChange={this.onChange}
                                     >
                                         <option value={0}>Select Priority</option>
                                         <option value={1}>High</option>
@@ -73,6 +98,7 @@ class AddProjectTask extends Component {
                                     <select className="form-control form-control-lg" 
                                         name="status"
                                         value={this.state.status}
+                                        onChange={this.onChange}
                                     >
                                         <option value="">Select Status</option>
                                         <option value="TO_DO">TO DO</option>
