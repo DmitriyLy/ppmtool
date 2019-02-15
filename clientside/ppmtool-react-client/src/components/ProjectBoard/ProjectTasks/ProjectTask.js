aac3ab4babca076c52edb19c218f3ac842a4da6c
DmitriyLy <dmitriy.lisay@gmail.com>
2019-02-15 19:49:04 +0200

    s5 l74

--- clientside/ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ clientside/ppmtool-react-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { deleteProjectTask } from '../../../actions/backlogActions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class ProjectTask extends Component {
+class ProjectTask extends Component {
+
+
+    onDeleteClick(backlog_id, pt_id) {
+        this.props.deleteProjectTask(backlog_id, pt_id);
+    }
+
     render() {
 
         const { project_task } = this.props;
@@ -38,7 +47,7 @@ export default class ProjectTask extends Component {
                         View / Update
                     </Link>
 
-                    <button className="btn btn-danger ml-4">
+                    <button className="btn btn-danger ml-4" onClick={this.onDeleteClick.bind(this, project_task.projectIdentifier, project_task.projectSequence)}>
                         Delete
                     </button>
                 </div>
@@ -47,4 +56,8 @@ export default class ProjectTask extends Component {
     }
 }
 
-//export default ProjectTask;
+ProjectTask.propTypes = {
+    deleteProjectTask: PropTypes.func.isRequired  
+};
+
+export default connect(null, {deleteProjectTask})(ProjectTask);
